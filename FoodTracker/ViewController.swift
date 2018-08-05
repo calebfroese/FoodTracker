f77f72c0a28eb2d9cd83b162b42f733bade91c55
Caleb Froese <caleb.froese@gmail.com>
2018-08-05 22:09:27 +0930

    Update label on button press

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Properties
+    @IBOutlet weak var nameTestField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // MARK: Actions
+    @IBOutlet weak var setDefaultLabelText: UIButton!
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
     }
 }
 
