7079d8bdd8bd7b0f1317efdaa8e5d365825bcdbc
Caleb Froese <caleb.froese@gmail.com>
2018-08-05 22:14:48 +0930

    Rename label after editing text field

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     // MARK: Properties
     @IBOutlet weak var nameTestField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        // Do any additional setup after loading the view, typically from a nib.
+        nameTestField.delegate = self
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
     // MARK: Actions
     @IBOutlet weak var setDefaultLabelText: UIButton!
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
