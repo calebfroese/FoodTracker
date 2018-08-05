3537115061daab7f79d6c2b9dc92508c1edac125
Caleb Froese <caleb.froese@gmail.com>
2018-08-05 22:40:19 +0930

    Add photo upload

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // MARK: Properties
     @IBOutlet weak var nameTestField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
@@ -31,10 +31,34 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mealNameLabel.text = textField.text
     }
     
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user cancelled
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image but was provided \(info)")
+        }
+        photoImageView.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     // MARK: Actions
     @IBOutlet weak var setDefaultLabelText: UIButton!
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
+    }
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Hide the keyboard
+        nameTestField.resignFirstResponder()
+        
+        let imageController = UIImagePickerController()
+        imageController.sourceType = .photoLibrary
+        imageController.delegate = self
+        present(imageController, animated: true, completion: nil)
     }
 }
 
